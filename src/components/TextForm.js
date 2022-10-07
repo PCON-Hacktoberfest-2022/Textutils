25fb13b8f4f819ee75d24af01a1cc144f96c401f
Ankit Kumar <ankitkumar3624@gmail.com>
2022-10-07 19:12:11 +0530

    dark mode ui changed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -70,20 +70,20 @@ export default function TextForm(props) {
         <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
             <h1 className='mb-4'>{props.heading}</h1>
             <div className="mb-3"> 
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#121212':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleFindReplace}>Find and Replace</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button disabled={text.length===0} className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleFindReplace}>Find and Replace</button>
 
             { findAndReplace && 
                 <div style={{display : 'flex', width : '200px',flexWrap: 'wrap'}}>
                     <input type="text" onChange={handleReplaceTextOnChange} className="form-control my-1" placeholder='Word in paragraph'/>
                     <input type="text" onChange={handleWithTextOnChange} className="form-control my-1" placeholder='Replace word with'/>
-                    <button className="btn btn-primary mx-1 mb-1 margin-top" onClick={handleReplaceClick}>Replace Instance</button>
+                    <button className={`btn btn-${props.mode} mx-1 my-1 margin-top`} onClick={handleReplaceClick}>Replace Instance</button>
                 </div>
             }
         </div>
