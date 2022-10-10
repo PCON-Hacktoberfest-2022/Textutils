5613b1e069da2bfaa06dfe71775f87f5784f3579
Gautam Adatra <gautamkiran123@gmail.com>
2022-10-10 15:20:26 +0530

    FEAT: updated the preview UI

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -99,12 +99,6 @@ export default function TextForm(props) {
     // setText("new text"); // Correct way to change the state
     return (
         <>
-
-       
-            
-         
-
-         
             <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
                 <h1 className='mb-4'>{props.heading}</h1>
                 <div className="mb-3">
@@ -130,12 +124,19 @@ export default function TextForm(props) {
                     </div>
                 }
             </div>
+                {/* added by Gautam2305 */}
+
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
-                <h2>Your text summary</h2>
-                <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
-                <p>{0.008 * text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} Minutes read</p>
-                <h2>Preview</h2>
-                <p>{text.length > 0 ? text : "Nothing to preview!"}</p>
+
+                <section >
+                <h2 className='preview-heading'>Preview</h2>
+                <p className='preview-container form-control'>{text.length > 0 ? text : "Nothing to preview!"}</p>
+                </section>
+                
+                <h3 className='preview-heading summ-heading'>Your text summary</h3>
+                <p className='text-update in-text-updt'>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words with {text.length} characters</p>
+                <p className='text-update'><span className='in-text-updt'>{0.008 * text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} Minutes read</span></p>
+                
             </div>
         </>
     )
