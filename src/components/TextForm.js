9dadad320534de0c67bb878f5123b87c037f2fb8
sinaps07 <sinaps3547@gmail.com>
2022-10-11 17:05:36 +0530

    fixed: the paste to text box

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -98,9 +98,9 @@ export default function TextForm(props) {
     textHistory.push(text);
     updateTextHistory(textHistory);
     setTimeout(async () => {
-      const text = await navigator.clipboard.readText();
-      setText(text);
-    }, 2000);
+      const textnew = await navigator.clipboard.readText();
+      setText(text + textnew);
+    }, 0);
     props.showAlert("Text Pasted from Clipboard", "success");
   };
 
