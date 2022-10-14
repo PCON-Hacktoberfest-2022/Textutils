3487d7debedc0edeadaa1b0e9a06bfd01ab75064
amann013 <coolaman4554@gmail.com>
2022-10-14 11:13:25 +0530

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export const Footer = (props) => {
   return (
     <footer className={`bg-${props.mode}`}>
-        <p className={`container text-center text-${props.mode === 'light' ? 'dark' : 'light'}`}> &copy; Copyright PCON Hactoberfest 2022</p>
+        <p className={`container text-center text-${props.mode === 'light' ? 'dark' : 'light'}`}> &copy; Copyright PCON Hacktoberfest 2022</p>
     </footer>
   )
 }
